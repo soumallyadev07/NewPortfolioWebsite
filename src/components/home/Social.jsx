f5d92684afbf4d61666274f1f80ed7632dd31714
soumallyadev07 <devsoumallya@gmail.com>
2022-12-22 12:19:51 +0530

    Initial Commit

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.google.com" className="home__social-icon" target="_blank">
+            <i className="uil uil-github-alt"></i>
+        </a>
+
+        <a href="https://www.google.com" className="home__social-icon" target="_blank">
+            <i className="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href="https://www.google.com" className="home__social-icon" target="_blank">
+            <i className="uil uil-instagram"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
