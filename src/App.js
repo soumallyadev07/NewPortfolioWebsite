978f081159a05a7f9dc642d0104c36cebd386187
soumallyadev07 <devsoumallya@gmail.com>
2022-12-25 10:26:11 +0530

    Pre-Loader

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, {useState, useEffect} from 'react';
+import PropagateLoader from "react-spinners/PropagateLoader";
+
 import './App.css';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
@@ -11,23 +14,57 @@ import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer';
 import ScrollUp from './components/scrollup/ScrollUp';
 
+const override = {
+  display: "block",
+  marginLeft: "50%",
+  marginRight: "50%",
+  marginTop: "20rem",
+  borderColor: "red",
+};
+
 function App() {
+  let [color, setColor] = useState("#000000");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 2800)
+  }, [])
+
   return (
     <>
-      <Header/>
-      <main className='main'>
-        <Home/>
-        <About/>
-        <Skills/>
-        <Services/>
-        <Qualifications/>
-        <Projects/>
-        <Inspirations/>
-        <Contact/>
-      </main>
-      
-      <Footer/>
-      <ScrollUp/>
+      {
+        loading ?
+
+        <PropagateLoader
+        color={color}
+        loading={loading}
+        cssOverride={override}
+        size={15}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+        />
+
+        :
+        <>
+          <Header/>
+          <main className='main'>
+            <Home/>
+            <About/>
+            <Skills/>
+            <Services/>
+            <Qualifications/>
+            <Projects/>
+            <Inspirations/>
+            <Contact/>
+          </main>
+          
+          <Footer/>
+          <ScrollUp/>
+        </>
+      }
     </>
   );
 }
